cde47647b7b7d900f7e3e5762c76ab3f79590f74
EstritPk <street.rojas69@gmail.com>
2022-06-04 01:00:56 -0400

    eje

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                     <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
                 </div>
                 <div class="form-group">
-                    <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
+                    <textarea name="description" rows="2" class="form-control" placeholder="Task Description aja "></textarea>
                 </div>
                 <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save Task, Mothafoka">
             </form>
