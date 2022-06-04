98caff58f0602c6ef831e4dd4490eb89b9b37d2b
EstritPk <street.rojas69@gmail.com>
2022-06-04 01:18:42 -0400

    desingtry

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <div class="card card-body">
             <form action="save_task.php" method="POST">
                 <div class="form-group">
-                    <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
+                    <input type="text" name="title" class="form-control" placeholder="Task Title de pana siono qliao" autofocus>
                 </div>
                 <div class="form-group">
                     <textarea name="description" rows="2" class="form-control" placeholder="Task Description aja eje "></textarea>
